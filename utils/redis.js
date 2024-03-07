325982c524940a4f27596e292b27957521779a7d
shalom ogoziem <shalomzymike@gmail.com>
2024-03-07 13:58:36 +0100

    db

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+import redis from 'redis';
+
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    // any error of the redis client must be displayed
+    // on the console
+    this.client.on('error', (err) => {
+      console.error(err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
